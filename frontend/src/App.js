5abb81e0dc779f0d1a1bd6dfa60ebbebf6bc0d17
Ashutosh R Dosi <ashutoshdosi@gmail.com>
2024-05-24 07:36:30 +0530

    Make first page

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import data from "./data";
+
+export default function App(){
+  return(
+    <div>
+      <header> <a href="/">amazona</a></header>
+      <main>
+        <h1>Feature Products</h1>
+        <div className ="products">
+          {
+            data.products.map(product=> (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+                </a>
+                <div className = "product-info">
+                  <a href = {`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                  </a>
+                  <p> <strong>${product.price }</strong> </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ) ) 
+          }
+        </div>
+      </main>
+    </div>
+  )
+}
